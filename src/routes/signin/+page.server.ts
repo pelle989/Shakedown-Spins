af6f34d4db892d980999b1e526e2b61d08bc6d8c
pelle989 <joe.kirchner@hotmail.com>
2026-05-18 18:52:46 -0400

    Guard signin and signout page routes

--- src/routes/signin/+page.server.ts
+++ src/routes/signin/+page.server.ts
@@ -1,5 +1,10 @@
+import { redirect } from '@sveltejs/kit';
 import { signIn } from '../../auth';
-import type { Actions } from './$types';
+import type { Actions, PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => {
+  throw redirect(303, '/');
+};
 
 export const actions: Actions = {
   default: signIn
